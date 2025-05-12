fc359cd0c58c027113a9f3bec5e6673f1ad2dc11
Djinnet <the.gaming.djinn@gmail.com>
2025-05-12 06:41:55 +0200

    Add selected colors mosaic option - and make a basic error msg system for now. (will expand that later version to other options)

---
+++ wwwroot/js/colorPicker.js
@@ -0,0 +1,36 @@
+﻿window.registerOutsideClick = function (dotNetHelper) {
+    document.addEventListener('click', function (e) {
+        const picker = document.querySelector('.color-picker-container');
+        if (picker && !picker.contains(e.target)) {
+            dotNetHelper.invokeMethodAsync('HideColorPicker');
+        }
+    });
+};
+
+window.unregisterOutsideClick = function () {
+    // Cleanup for disposal
+    const picker = document.querySelector('.color-picker-container');
+    if (picker) {
+        picker.removeEventListener('click', function (e) {
+            const picker = document.querySelector('.color-picker-container');
+            if (picker && !picker.contains(e.target)) {
+                dotNetHelper.invokeMethodAsync('HideColorPicker');
+            }
+        });
+    }
+};
+
+window.positionAbove = (pickerId, slotId) => {
+    const picker = document.getElementById(pickerId);
+    const slot = document.getElementById(slotId);
+
+    if (!picker || !slot) return;
+
+    //const slotRect = slot.getBoundingClientRect();
+    //const parentRect = slot.offsetParent.getBoundingClientRect();
+
+    picker.style.position = "absolute";
+    picker.style.top = (slot.offsetTop - picker.offsetHeight - 10) + "px";
+    picker.style.left = slot.offsetLeft + "px";
+};
+
